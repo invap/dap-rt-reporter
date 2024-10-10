3aa0227a9103eb47711789e147012b24c2836931
Juan Ignacio Vaccarezza <jivaccarezza@invap.com.ar>
2024-10-10 09:25:54 -0300

    Add blank line after license

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # Copyright (C) <2024>  INVAP S.E.
 # SPDX-License-Identifier: AGPL-3.0-or-later
+#
 # Test file for add_executable and execute methods.
 # Expected behaviour is:
 # First response is capabilities from GDB
@@ -16,3 +17,4 @@ if __name__ == "__main__":
     print(response)
     response = reporter.execute()
     print(response)
+
