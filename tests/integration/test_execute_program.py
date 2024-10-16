d799cae5e9dfee9675dd2429d2963965105bada2
Juan Ignacio Vaccarezza <jivaccarezza@invap.com.ar>
2024-10-16 14:23:47 -0300

    Add __pycache__

--- tests/integration/test_execute_program.py
+++ tests/integration/test_execute_program.py
@@ -10,22 +10,28 @@ import unittest
 from src.connection_wrapper import ConnectionWrapper
 from src.reporter import Reporter
 
+
 class TestExecuteProgram(unittest.TestCase):
     def test_start(self):
         self.connection = ConnectionWrapper()
         self.reporter = Reporter(self.connection)
 
         # Set main binary as executable, log file is not used
-        self.reporter.add_executable("resources/simple_test/target/debug/simple_test", "resources/main_log_file.log")
+        self.reporter.add_executable(
+            "resources/simple_test/target/debug/simple_test",
+            "resources/main_log_file.log",
+        )
 
         response = self.reporter.execute()
         response = response.decode()
 
         self.connection.close_connection()
 
-        #Checks if response contains a terminated event
-        self.assertIn("{\"type\": \"event\", \"event\": \"exited\", \"body\": {\"exitCode\": 0}, ", response)
+        # Checks if response contains a terminated event
+        self.assertIn(
+            '{"type": "event", "event": "exited", "body": {"exitCode": 0}, ', response
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
-
