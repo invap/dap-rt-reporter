b929a5f590341f7b453e718ec987430b46bb40bb
AVeizaga <alvedi66@gmail.com>
2024-11-26 19:55:13 -0300

    task_finished event added

--- tests/integration/test_checkpoint_reached.py
+++ tests/integration/test_checkpoint_reached.py
@@ -27,7 +27,7 @@ class TestCheckpointReached(unittest.TestCase):
         print("---------")
         print(f"Executing: {sut} output to: {log_path}")
         self.reporter.execute()
-        self.reporter.stop()
+        self.reporter.close()
 
 
 if __name__ == "__main__":
