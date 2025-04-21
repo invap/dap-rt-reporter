88b41bbcec30a57659283a71b12528726fb26668
AVeizaga <abveizaga@invap.com.ar>
2025-04-21 12:43:00 +0000

    Modifications to support toml config

--- dap_rt_reporter/__main__.py
+++ dap_rt_reporter/__main__.py
@@ -4,6 +4,8 @@
 import csv
 import argparse
 import os
+import tomllib
+import time
 
 from dap_rt_reporter.types import ReportEvent
 from dap_rt_reporter.reporter import Reporter
@@ -19,6 +21,126 @@ from dap_rt_reporter.event.clock_reset import ClockResetEvent
 from dap_rt_reporter.event.clock_resume import ClockResumeEvent
 from dap_rt_reporter.event.component_event import ComponentEvent
 
+def read_toml(toml_path):
+    with open(toml_path, "rb") as file:
+        reporter_config = tomllib.load(file)["dap-rt-reporter"]
+        sut = reporter_config["sut"]
+        config_file = reporter_config["specification"]
+    
+    return sut, config_file
+
+
+def run_experiment(sut, config_file, log_path):
+    reporter = Reporter(sut, log_path)
+
+    # Read each line and add corresponding events
+    with open(config_file, "r") as workflow_file:
+        workflow_reader = csv.reader(workflow_file, delimiter=",")
+
+        for row in workflow_reader:
+            # Split the breakpoint descriptor
+            source_path, line, before = row[0].split(":")
+            before = before == "b"
+
+            event = row[1]
+            event_name = row[2]
+
+            args = []
+            if len(row) > 3:
+                args = row[3:]
+            match event:
+                case ReportEvent.CHECKPOINT_REACHED:
+                    reporter.set_event(
+                        CheckpointReachedEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                        )
+                    )
+                case ReportEvent.TASK_STARTED:
+                    reporter.set_event(
+                        TaskStartedEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                        )
+                    )
+                case ReportEvent.TASK_FINISHED:
+                    reporter.set_event(
+                        TaskFinishedEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                        )
+                    )
+                case ReportEvent.VARIABLE_VALUE_ASSIGNED:
+                    reporter.set_event(
+                        VariableValueAssignedEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                            expression=args[0],
+                        )
+                    )
+                case ReportEvent.CLOCK_START:
+                    reporter.set_event(
+                        ClockStartEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                        )
+                    )
+                case ReportEvent.CLOCK_PAUSE:
+                    reporter.set_event(
+                        ClockPauseEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                        )
+                    )
+                case ReportEvent.CLOCK_RESUME:
+                    reporter.set_event(
+                        ClockResumeEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                        )
+                    )
+                case ReportEvent.CLOCK_RESET:
+                    reporter.set_event(
+                        ClockResetEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                        )
+                    )
+                case ReportEvent.COMPONENT_EVENT:
+                    reporter.set_event(
+                        ComponentEvent(
+                            source_path=source_path,
+                            line=int(line),
+                            before=before,
+                            name=event_name,
+                            function_name=args[0],
+                            function_params=args[1:],
+                        )
+                    )
+                case _:
+                    raise RuntimeError(f"Event {event} is undefined.")
+
+    reporter.execute()
+
+    reporter.close()
+
+
 # Parser arguments
 parser = argparse.ArgumentParser(
     prog="dap_reporter",
@@ -28,8 +150,9 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("--sut", help="binary of the program to report", required=True)
 parser.add_argument("--desc", help="configuration file", required=True)
-parser.add_argument("--log", help="log file to store report", required=True)
+parser.add_argument("--log", help="log file to store report")
 parser.add_argument("-f", help="force log rewrite", action="store_true")
+parser.add_argument("--toml", help="Configure all reporter options with a toml file.")
 
 args = parser.parse_args()
 
@@ -37,120 +160,31 @@ sut = args.sut
 config_file = args.desc
 log_path = args.log
 force = args.f
+toml_path = args.toml    
 
-# Checks
-if not os.path.isfile(sut):
-    raise RuntimeError(f"No file named {sut} exists.")
-if not os.path.isfile(config_file):
-    raise RuntimeError(f"No file named {config_file} exists.")
-if os.path.isfile(log_path) and not force:
-    raise RuntimeError(f"Warning: {log_path} already exists, use -f to force rewrite.")
+# toml override
+if toml_path:
+    # Check if path is valid
+    if not (os.path.isfile(toml_path) or os.path.isdir(toml_path)):
+        raise RuntimeError(f"No file or folder named {toml_path} exists.")
 
-reporter = Reporter(sut, log_path)
+    if os.path.isfile(toml_path):
+        sut, config_file = read_toml()
+        log_path = "experiments/" + config_file + "/" + str(time.time) + "/exe-report.log"
+        run_experiment(sut, config_file, log_path)
+    else:
+        for root, dirs, files in os.walk(toml_path):
+            for file in files:
+                if file.endswith(".toml"):
+                    sut, config_file = read_toml(file)
+                    log_path = "experiments/" + config_file + "/" + str(time.time) + "/exe-report.log"
+                    run_experiment(sut, config_file, log_path)
+else:
+    if not os.path.isfile(sut):
+        raise RuntimeError(f"No file named {sut} exists.")
+    if not os.path.isfile(config_file):
+        raise RuntimeError(f"No file named {config_file} exists.")
+    if os.path.isfile(log_path) and not force:
+        raise RuntimeError(f"Warning: {log_path} already exists, use -f to force rewrite.")
 
-# Read each line and add corresponding events
-with open(config_file, "r") as workflow_file:
-    workflow_reader = csv.reader(workflow_file, delimiter=",")
-
-    for row in workflow_reader:
-        # Split the breakpoint descriptor
-        source_path, line, before = row[0].split(":")
-        before = before == "b"
-
-        event = row[1]
-        event_name = row[2]
-
-        args = []
-        if len(row) > 3:
-            args = row[3:]
-        match event:
-            case ReportEvent.CHECKPOINT_REACHED:
-                reporter.set_event(
-                    CheckpointReachedEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                    )
-                )
-            case ReportEvent.TASK_STARTED:
-                reporter.set_event(
-                    TaskStartedEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                    )
-                )
-            case ReportEvent.TASK_FINISHED:
-                reporter.set_event(
-                    TaskFinishedEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                    )
-                )
-            case ReportEvent.VARIABLE_VALUE_ASSIGNED:
-                reporter.set_event(
-                    VariableValueAssignedEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                        expression=args[0],
-                    )
-                )
-            case ReportEvent.CLOCK_START:
-                reporter.set_event(
-                    ClockStartEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                    )
-                )
-            case ReportEvent.CLOCK_PAUSE:
-                reporter.set_event(
-                    ClockPauseEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                    )
-                )
-            case ReportEvent.CLOCK_RESUME:
-                reporter.set_event(
-                    ClockResumeEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                    )
-                )
-            case ReportEvent.CLOCK_RESET:
-                reporter.set_event(
-                    ClockResetEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                    )
-                )
-            case ReportEvent.COMPONENT_EVENT:
-                reporter.set_event(
-                    ComponentEvent(
-                        source_path=source_path,
-                        line=int(line),
-                        before=before,
-                        name=event_name,
-                        function_name=args[0],
-                        function_params=args[1:],
-                    )
-                )
-            case _:
-                raise RuntimeError(f"Event {event} is undefined.")
-
-reporter.execute()
-
-reporter.close()
+    run_experiment(sut, config_file, log_path)
