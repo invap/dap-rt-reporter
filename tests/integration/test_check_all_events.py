81ba65b4a9b70218ebc0e35295f2f7d7798c8c23
AVeizaga <abveizaga@invap.com.ar>
2025-04-28 12:22:35 +0000

    Include report log checking in tests

--- tests/integration/test_check_all_events.py
+++ tests/integration/test_check_all_events.py
@@ -5,11 +5,12 @@
 # Run the example program using every type of event.
 # Check if output file is consistent with the events.
 
-import unittest
 import csv
 import subprocess
+import unittest
 
-from dap_rt_reporter.types import ReportEventType, ReportEvent
+from dap_rt_reporter.types import ReportEvent, ReportEventType
+
 
 class TestCheckAllEvents(unittest.TestCase):
     def test_events(self):
@@ -17,8 +18,9 @@ class TestCheckAllEvents(unittest.TestCase):
             "tests/integration/resources/simple_test/target/debug/simple_test"
         )
         conf_file = "tests/integration/resources/simple_test_config.csv"
-        execution_log = "tests/integration/resources/all_events.log"
+        execution_log = "tests/integration/all_events.log"
 
+        # Run dap-rt-reporter
         subprocess.run(
             [
                 "pipx",
@@ -34,38 +36,107 @@ class TestCheckAllEvents(unittest.TestCase):
                 conf_file,
                 "--log",
                 execution_log,
-                "-f"
+                "-f",
             ]
         )
 
+        # Build expected log
         events = []
 
         x = 1
         y = 1
-        events.append([ReportEventType.STATE_EVENT, ReportEvent.VARIABLE_VALUE_ASSIGNED, "var_x", str(x)])
-        events.append([ReportEventType.STATE_EVENT, ReportEvent.VARIABLE_VALUE_ASSIGNED, "var_y", str(y)])
-        events.append([ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_START, "sleep_clk"])
-        events.append([ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_PAUSE, "sleep_clk"])
+        events.append(
+            [
+                ReportEventType.STATE_EVENT,
+                ReportEvent.VARIABLE_VALUE_ASSIGNED,
+                "var_x",
+                str(x),
+            ]
+        )
+        events.append(
+            [
+                ReportEventType.STATE_EVENT,
+                ReportEvent.VARIABLE_VALUE_ASSIGNED,
+                "var_y",
+                str(y),
+            ]
+        )
+        events.append(
+            [ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_START, "sleep_clk"]
+        )
+        events.append(
+            [ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_PAUSE, "sleep_clk"]
+        )
         for i in range(10):
-            events.append([ReportEventType.PROCESS_EVENT, ReportEvent.TASK_STARTED, "loop"])
+            events.append(
+                [
+                    ReportEventType.STATE_EVENT,
+                    ReportEvent.VARIABLE_VALUE_ASSIGNED,
+                    "var_i",
+                    str(i),
+                ]
+            )
+            events.append(
+                [ReportEventType.PROCESS_EVENT, ReportEvent.TASK_STARTED, "loop"]
+            )
             x *= 2
             y *= 3
-            events.append([ReportEventType.STATE_EVENT, ReportEvent.VARIABLE_VALUE_ASSIGNED, "var_x", str(x)])
-            events.append([ReportEventType.STATE_EVENT, ReportEvent.VARIABLE_VALUE_ASSIGNED, "var_y", str(y)])
-            events.append([ReportEventType.PROCESS_EVENT, ReportEvent.CHECKPOINT_REACHED, "chk_sleep"])
-            events.append([ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_RESET, "sleep_clk"])
-            events.append([ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_RESUME, "sleep_clk"])
-            events.append([ReportEventType.COMPONENT_EVENT, "component", "component_func", str(x), str(y)])
-            events.append([ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_PAUSE, "sleep_clk"])
-            events.append([ReportEventType.PROCESS_EVENT, ReportEvent.TASK_FINISHED, "loop"])
+            events.append(
+                [
+                    ReportEventType.STATE_EVENT,
+                    ReportEvent.VARIABLE_VALUE_ASSIGNED,
+                    "var_x",
+                    str(x),
+                ]
+            )
+            events.append(
+                [
+                    ReportEventType.STATE_EVENT,
+                    ReportEvent.VARIABLE_VALUE_ASSIGNED,
+                    "var_y",
+                    str(y),
+                ]
+            )
+            events.append(
+                [
+                    ReportEventType.PROCESS_EVENT,
+                    ReportEvent.CHECKPOINT_REACHED,
+                    "loop_inv_chk",
+                ]
+            )
+            events.append(
+                [
+                    ReportEventType.COMPONENT_EVENT,
+                    "component",
+                    "component_func",
+                    str(x),
+                    str(y),
+                ]
+            )
+            events.append(
+                [ReportEventType.PROCESS_EVENT, ReportEvent.TASK_FINISHED, "loop"]
+            )
+            events.append(
+                [ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_RESET, "sleep_clk"]
+            )
+            events.append(
+                [ReportEventType.TIMED_EVENT, ReportEvent.CLOCK_PAUSE, "sleep_clk"]
+            )
+            events.append(
+                [
+                    ReportEventType.PROCESS_EVENT,
+                    ReportEvent.CHECKPOINT_REACHED,
+                    "chk",
+                ]
+            )
 
+        # Test if log matches
         with open(execution_log) as log:
             csv_reader = csv.reader(log)
 
-            it = 0
-            for row in csv_reader:
-                self.assertTrue(row[1:] == events[it])
-                it += 1
+            for row, event in zip(csv_reader, events):
+                self.assertTrue(row[1:] == event)
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/integration/test_checkpoint_reached.py
+++ tests/integration/test_checkpoint_reached.py
@@ -2,39 +2,63 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import unittest
+import csv
 
 from dap_rt_reporter.reporter import Reporter
 from dap_rt_reporter.event.checkpoint_reached_event import CheckpointReachedEvent
+from dap_rt_reporter.types import ReportEvent, ReportEventType
 
 
 class TestCheckpointReached(unittest.TestCase):
     def test_checkpoint(self):
         sut = "tests/integration/resources/simple_test/target/debug/simple_test"
-        log_path = "main_log_file.log"
+        execution_log = "tests/integration/main_log_file.log"
 
-        self.reporter = Reporter(sut, log_path)
+        self.reporter = Reporter(sut, execution_log)
 
+        # Set checkpoints
         self.reporter.set_event(
             CheckpointReachedEvent(
-                source_path="main.rs", line=7, before=True, name="chk_7"
+                source_path="main.rs", line=12, before=True, name="chk_0"
             )
         )
 
         self.reporter.set_event(
             CheckpointReachedEvent(
-                source_path="main.rs", line=9, before=False, name="chk_9"
+                source_path="main.rs", line=16, before=False, name="chk_1"
             )
         )
         self.reporter.set_event(
             CheckpointReachedEvent(
-                source_path="main.rs", line=11, before=False, name="chk_11"
+                source_path="main.rs", line=25, before=False, name="chk_2"
             )
         )
 
-        print("---------")
-        print(f"Executing: {sut} output to: {log_path}")
         self.reporter.execute()
         self.reporter.close()
+
+        # Build expected log
+        events = []
+
+        events.append(
+            [ReportEventType.PROCESS_EVENT, ReportEvent.CHECKPOINT_REACHED, "chk_0"]
+        )
+
+        for i in range(10):
+            events.append(
+                [ReportEventType.PROCESS_EVENT, ReportEvent.CHECKPOINT_REACHED, "chk_1"]
+            )
+
+        events.append(
+            [ReportEventType.PROCESS_EVENT, ReportEvent.CHECKPOINT_REACHED, "chk_2"]
+        )
+
+        # Test if log matches
+        with open(execution_log) as log:
+            csv_reader = csv.reader(log)
+
+            for row, event in zip(csv_reader, events):
+                self.assertTrue(row[1:] == event)
 
 
 if __name__ == "__main__":
