1221de853244dfc3601a49d48a203e8c08c6fc65
AVeizaga <abveizaga@invap.com.ar>
2025-09-15 17:48:11 +0000

    Remove server config and redifine event server connection

--- dap_rt_reporter/rabbitmq_connection/rabbitmq_server_configs.py
+++
@@ -1,4 +0,0 @@
-from rt_rabbitmq_wrapper.rabbitmq_utility import RabbitMQ_server_config, RabbitMQ_exchange_config
-
-rabbitmq_server_config = RabbitMQ_server_config()
-rabbitmq_event_exchange_config = RabbitMQ_exchange_config()
