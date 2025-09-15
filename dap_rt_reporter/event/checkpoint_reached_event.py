0bf9f2d9662507718343647fc22ec7245193a824
AVeizaga <abveizaga@invap.com.ar>
2025-09-15 17:55:58 +0000

    Add docstrings to variable value assigned and checkpoint reached

--- dap_rt_reporter/event/checkpoint_reached_event.py
+++ dap_rt_reporter/event/checkpoint_reached_event.py
@@ -4,9 +4,21 @@
 from dap_rt_reporter.event.process_event import ProcessEvent
 from dap_rt_reporter.types import ReportEventSubType
 
-class CheckpointReachedEvent(ProcessEvent):
-    """Checkpoint reached event class."""
 
-    def __init__(self, source_path, line, before, name):
+class CheckpointReachedEvent(ProcessEvent):
+    """Checkpoint reached event."""
+
+    def __init__(
+        self, source_path: str, line: int, before: bool, name: str
+    ) -> None:
+        """Initialize new checkpoint reached event.
+
+        Args:
+            source_path (str): Source path asociated with the event.
+            line (int): Line asociated with the event.
+            before (bool): Select if event should report before a line is executed.
+            name (str): Name for the event.
+        """
+
         super().__init__(source_path, line, before, name)
         self._set_sub_type(ReportEventSubType.CHECKPOINT_REACHED)
